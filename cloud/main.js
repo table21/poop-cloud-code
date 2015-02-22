b1406bc05b3edc4cafa8242a1f6e19a883b15d1a
Dzmitry Tsurankou <dima.tsurankou@gmail.com>
2015-02-21 16:16:57 -0800

    Revert "yay"

--- cloud/main.js
+++ cloud/main.js
@@ -1,54 +1,36 @@
 
 Parse.Cloud.define("restrooms", function(request, response) {
+
   Parse.Cloud.httpRequest({
     url: 'http://www.refugerestrooms.org/api/v1/restrooms/by_location.json',
     params: {
       lat : request.params.lat,
-      lng : request.params.lng
+      lng : request.params.lng,
     },
     success: function(httpResponse) {
-      var pointList = httpResponse.data;
-      var idList = pointList.map(function(toilet) { return ("refugerestrooms_" + toilet.id); });
-      var query = new Parse.Query("RefugeComment");
-      query.containedIn("comment_id", idList);
-      query.find({
-        success: function(commentList) {
-          var bathroomData = httpResponse.data.map(function(toilet) {
-            var current = {};
-            var currentLocation = new Parse.GeoPoint({
-              latitude: toilet.latitude, 
-              longitude: toilet.longitude 
-            });
-            current.location = currentLocation;
+      var bathroomData = httpResponse.data;
 
-            current["name"] = toilet.name;
-            current["id"] = "refugerestrooms_" + toilet.id;
-            current["features"] = {
-              "disabled": toilet.accessible
-            };
-            current["comments"] = response;
+      bathroomData = bathroomData.map(function(toilet) {
 
-            return current;
-          });
+        var toiletLocation = new Parse.GeoPoint({latitude: request.params.lat, longitude: request.params.lng });
+        toilet.location = toiletLocation;
 
-          var comments = {};
-          commentList.forEach(function(comment) { 
-            if (!comments[comment.attributes["comment_id"]]) {
-              comments[comment.attributes["comment_id"]] = [];
-            }
-            comments[comment.attributes["comment_id"]].push(comment.attributes["text"]);
-          });
+        var reviews = new Parse.Query("Review");
+        reviews.equalTo("location", toiletLocation);
+        reviews.find({
+          success: function(results) {
+            toilet.reviews = results;
+          },
+          error: function() {
+            // response.error("location lookup failed");
+          }
+        });
 
-          bathroomData.map(function(toilet) {
-            toilet.comments = comments[toilet.id];
-          });
+        return toilet;
 
-          response.success(bathroomData);
-        },
-        error: function(myObject, error) {
-          response.error(error);
-        }
-      });
+      })
+
+      response.success(bathroomData);
     },
     error: function(httpResponse) {
       response.error('Request failed with response code ' + httpResponse.status);
